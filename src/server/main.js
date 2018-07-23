1521dc5a58ed51adb83e50f3746b4c726200006e
Lcharvol <charvolin0@gmail.com>
2018-07-23 17:01:42 +0200

    WIP

--- src/server/main.js
+++ src/server/main.js
@@ -3,4 +3,6 @@ import params  from '../../params';
 import * as server from './index';
 import debug from 'debug';
 
-server.create(params.server).then( () => console.log('Server started') )
+server.create(params.server).then(({ stop }) => {
+
+});
