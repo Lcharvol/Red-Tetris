6ed003af190d9f690c5b443f411d0cd35f92d5b1
Lcharvol <charvolin0@gmail.com>
2018-07-28 15:26:01 +0200

    Enhance room managment

--- src/server/http/eventListener.js
+++ src/server/http/eventListener.js
@@ -1,4 +1,4 @@
-import { findIndex, propEq, isNil, length } from 'ramda';
+import { findIndex, propEq, isNil, length, find, contains, remove } from 'ramda';
 import debug from 'debug';
 import uuidv1 from 'uuid/v1';
 
@@ -9,23 +9,17 @@ import {
     moveRight,
     moveLeft,
 } from '../boardManager';
-import { TOAST_DURATION } from '../constants/game';
+import { removeToast, emitToRoom, emitToSocket } from './utils';
 
 const logger = debug('tetris:http');
+const roomLogger = debug('tetris:room');
 
 let rooms = [];
-
-const removeToast = (io, room) => setTimeout(() => io.to(room).emit('action', { name: 'removeToast'}), TOAST_DURATION);
-
-const emitToRoom = (io, room, type, name, body) => io.to(room).emit(type, { name, body });
-
-const emitToSocket = (socket, type, name, message) =>  socket.emit(type, { name, message });
 
 const eventListener = (socket, io) => {
     const currentSocketId = [];
     const socketIdToDelete = [];
     const tmp = [];
-
     currentSocketId[0] = socket.id;
     logger("Socket connected: " + currentSocketId);
 
@@ -33,13 +27,31 @@ const eventListener = (socket, io) => {
         .on('disconnect', async () => {
             logger("Socket disconnected: " + currentSocketId)
             socketIdToDelete[0] = socket.id;
+            let roomIndex = undefined;
+            let user = undefined;
+            rooms.map((room, id) => {
+                let userIndex = findIndex(propEq('id', socketIdToDelete[0]))(room.users);
+                if(userIndex >= 0) {
+                    user = rooms[id].users[userIndex];
+                    roomLogger(`${user.name} leave the ${rooms[id].name} room`);
+                    socket.leave(rooms[id].name);
+                    const newUsers = remove(userIndex, 1, rooms[id].users);
+                    rooms[id].users = newUsers;
+                    emitToRoom(io, rooms[id].name, 'action', 'updateGameInfo', { name: room, users: newUsers, toast: { id: uuidv1(), message:`${user.name} leave the room`} });
+                }
+            })
         })
-        .on('joinRoom', function(data) {
+        .on('joinRoom', async data => {
             const { room, user } = data;
             const roomIndex = findIndex(propEq('name', room))(rooms);
             if(roomIndex >= 0 && length(rooms[roomIndex].users) >= 2) {
-                emitToSocket(socket, 'gameError', 'fullRoom', 'This room is full')
-                logger('Too many player in the room');    
+                const userAllreadyIn = !isNil(find(propEq('name', user))(rooms[roomIndex].users));
+                if(userAllreadyIn) {
+                    roomLogger(`${user} try to join the ${room} room, but he's allready in`);
+                } else {
+                    emitToSocket(socket, 'gameError', 'fullRoom', 'This room is full')
+                    roomLogger('Too many player in the room');
+                }
             } else {
                 socket.join(room);
                 const users = !isNil(rooms[roomIndex]) ? [...rooms[roomIndex].users, {name: user, owner: false, id: currentSocketId[0], board: initialBoard}] : [{name: user, owner: true, id: currentSocketId[0], board: initialBoard}];
@@ -47,10 +59,10 @@ const eventListener = (socket, io) => {
                 else rooms[roomIndex] = {...rooms[roomIndex], users, name: room};   
                 emitToRoom(io, room, 'action', 'updateGameInfo', { name: room, users, toast: { id: uuidv1(), message:`${user} join the room`} });
                 removeToast(io, room);
-                logger(`${user} join the ${room} room`);
+                roomLogger(`${user} join the ${room} room`);
             }
         })
-        .on('action', function(actionSocket) {
+        .on('action', async actionSocket => {
             const roomIndex = findIndex(propEq('name', actionSocket.gameName))(rooms);
             if(actionSocket.name === 'startGame') {
                 if(roomIndex < 0) return;
